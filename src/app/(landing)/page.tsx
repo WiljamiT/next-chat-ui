cedee26bcc5ffd8291e55f6f6bda1e6ccfcc7416
WiljamiT <74732018+WiljamiT@users.noreply.github.com>
2024-11-04 14:46:24 +0200

    minor changes ui

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -4,8 +4,13 @@ import React from 'react';
 const page = () => {
   return (
     <div className="px-20 min-h-screen flex flex-col">
-      <main className="h-[30vh]">
-        <h1>Welcome</h1>
+      <main className="h-[30vh] text-center flex flex-col align-center">
+        <h1 className="text-4xl md:text-6xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-blue-600 to-purple-400 py-10">
+          AI avustaja
+        </h1>
+        <h2 className="text-xl md:text-2xl text-gray-500">
+          Kommunikoi älykkäästi ja luo upeita kuvia - kaikki yhdessä paikassa
+        </h2>
       </main>
       <div className="h-[70vh]">
         <BentoDemo />
--- src/components/Chat/BentoContainer.tsx
+++ src/components/Chat/BentoContainer.tsx
@@ -5,30 +5,30 @@ import { LightbulbIcon, SparklesIcon } from 'lucide-react';
 const features = [
   {
     Icon: SparklesIcon,
-    name: 'Key Features',
+    name: 'Ominaisuudet',
     description:
       'Powered by OpenAIs latest models High-quality image generation Natural language understanding Real-time responses.',
     href: '/',
     cta: 'Learn more',
     background: (
       <img
-        className="absolute opacity-60"
-        src="https://static1.cbrimages.com/wordpress/wp-content/uploads/2022/03/thanos-and-sacred-timeline.jpg?q=70&fit=crop&w=1140&h=&dpr=1"
+        className="absolute opacity-80"
+        src="https://media.licdn.com/dms/image/D4E12AQH20XVImOTUxA/article-cover_image-shrink_720_1280/0/1684178552574?e=2147483647&v=beta&t=1UTjTSCCFikmeNfs5dP0QFZRKWU0MzosJxSDd-_aNx8"
       />
     ),
     className: 'lg:row-start-1 lg:row-end-4 lg:col-start-2 lg:col-end-3',
   },
   {
     Icon: ChatBubbleIcon,
-    name: 'AI Chat Assistant',
+    name: 'AI Chat Apuri',
     description:
       'Engage in natural conversations with our advanced AI chatbot. Get answers, brainstorm ideas, or just chat about any topic.',
     href: '/',
     cta: 'Learn more',
     background: (
       <img
-        className="absolute opacity-60 transform -translate-y-20" // Added transform -translate-y-20
-        src="https://static1.cbrimages.com/wordpress/wp-content/uploads/2022/03/thanos-and-sacred-timeline.jpg?q=70&fit=crop&w=1140&h=&dpr=1"
+        className="absolute opacity-80 transform -translate-y-40" // Added transform -translate-y-20
+        src="https://s3-alpha.figma.com/hub/file/5897587322/52b9ba3b-b287-4ab8-aaed-f2d8a7acded8-cover.png"
       />
     ),
     className: 'lg:col-start-1 lg:col-end-2 lg:row-start-1 lg:row-end-3',
@@ -55,14 +55,14 @@ const features = [
   },
   {
     Icon: ImageIcon,
-    name: 'Image Generation',
+    name: 'Kuvien generointi',
     description:
       'Transform your ideas into stunning images. Create artwork, designs, and visuals using cutting-edge AI technology.',
     href: '/',
     cta: 'Learn more',
     background: (
       <img
-        className="absolute opacity-60 transform -translate-y-20" // Added transform -translate-y-20
+        className="absolute opacity-80 transform -translate-y-20" // Added transform -translate-y-20
         src="https://static1.cbrimages.com/wordpress/wp-content/uploads/2022/03/thanos-and-sacred-timeline.jpg?q=70&fit=crop&w=1140&h=&dpr=1"
       />
     ),
